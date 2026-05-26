1ca1a74b14c6c951b88b05e3ab008ed676b41e25
“Davide-SP” <77718226+Davide-SP@users.noreply.github.com>
2026-05-26 10:38:08 +0200

    removed location in the header section

--- animation/01/script.js
+++ animation/01/script.js
@@ -2,4 +2,20 @@ gsap.from(".letter", {
     y: -500, 
     duration: 2, 
     ease: "bounce.out",
-    stagger: 0.1})
+    stagger: 0.1
+})
+
+gsap.to(".letter", { 
+    scaleY: 0.6, 
+    // scaleX: 1.4,
+    duration: 0.1, 
+    delay: 2
+})
+
+gsap.to(".letter", { 
+    scaleY: 1, 
+    // scaleX: 1,
+    duration: 0.1, 
+    delay: 2.1
+})
+
